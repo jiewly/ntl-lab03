f04f6f6506e3574182cf4cde971339ac733d9feb
Irin Krapookthong (NTL) <CF303520@tidlor.com>
2021-07-12 09:43:33 +0700

    init

---
+++ Lab04/Operation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    public class Operation
+    {
+        public double num1; //properties
+        public double num2;//properties
+        public string operation;//properties
+        public Operation(double num1, double num2, string operation)//constructor
+        {
+            this.num1 = num1;//Dependency
+            this.num2 = num2;//Dependency
+            this.operation = operation;//Dependency
+        }
+        public double Execute()
+        {
+
+            if (operation == "+")//plus symbol
+            {
+                return this.num1 + this.num2;
+            }
+            else if (this.operation == "-")//minus sign
+            {
+                return this.num1 - this.num2;
+            }
+            else if (this.operation == "*")//multiplication sign
+            {
+                return this.num1 * this.num2;
+            }
+            else if (this.operation == "/")//division sign
+            {
+                return this.num1 / this.num2;
+            }
+            else
+            {
+                throw new Exception("***Error***");//Prevent error information
+            }
+            
+        }
+    }
+}
---
+++ Lab04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please input number A Operation(+,-,*,/) B ");
+            Console.WriteLine("------------------------- ");
+
+            Console.Write("Input number A : ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input number B : ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input operation (+,-,*,/) : ");
+            var mathematical = Console.ReadLine();
+
+            var operationcal = new Operation(num1, num2, mathematical);
+
+            var result = operationcal.Execute();
+
+            Console.WriteLine($"Summary: " + result);
+            Console.ReadLine();
+        }
+    }
+}
